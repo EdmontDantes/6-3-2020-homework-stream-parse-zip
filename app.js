8266f1466e1d6f5270d7e1c5b9afe7c2a4d1089a
Bogdan Kowaltchook <bogdan@En760.kowaltchook.com>
2020-06-07 23:37:07 -0400

    finished

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const zlib = require('zlib');
 const maleResults = [];
 const femaleResults = [];
 
-const writeStreamMales = fs.createWriteStream(__dirname + '/males.json', 'utf8')
-const writeStreamFemales = fs.createWriteStream(__dirname + '/females.json', 'utf8')
+const writeStreamMales = fs.createWriteStream(__dirname + '/males.json', 'utf8');
+const writeStreamFemales = fs.createWriteStream(__dirname + '/females.json', 'utf8');
 
 fs.createReadStream('data.csv')
     .pipe(csv())
@@ -25,36 +25,57 @@ fs.createReadStream('data.csv')
         writeStreamFemales.write(JSON.stringify(femaleResults));
     });
 
+const gzip = zlib.createGzip();
+const rwriteStreamMalesZipped = fs.createReadStream(__dirname + '/males.json');
+const wwriteStreamMalesZipped = fs.createWriteStream(__dirname + '/males.json.gz');
+rwriteStreamMalesZipped
+    .pipe(gzip)
+    .pipe(wwriteStreamMalesZipped)
+    .on('finish', () => console.log('done compressing'))
+
+const rwriteStreamFemalesZipped = fs.createReadStream(__dirname + '/females.json');
+const wwriteStreamFemalesZipped = fs.createWriteStream(__dirname + '/females.json.gz');
+rwriteStreamFemalesZipped
+    .pipe(gzip)
+    .pipe(wwriteStreamFemalesZipped)
+    .on('finish', () => console.log('done compressing'))
+
+
 const server = http.createServer((req, res) => {
 
     if (req.url === '/') {
         res.writeHead(200, {
             'Content-Type': 'text/html',
         });
-        const readStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+        const readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
         readStream.pipe(res);
-
-    } else if (req.url === '/') {
+    } else if (req.url === '/males') {
             res.writeHead(200, { 
-                'Content-Type': 'text/html' 
+                'Content-Type': 'application/json' 
             });
-            const readStream = fs.createReadStream(__dirname + '/males.json', 'utf8')
+            const readStream = fs.createReadStream(__dirname + '/males.json', 'utf8');
             readStream.pipe(res);
     } else if (req.url === '/females') {
-
             res.writeHead(200, { 
-                'Content-Type': 'text/html'
+                'Content-Type': 'application/json'
             });
-            const readStream = fs.createReadStream(__dirname + '/females.json', 'utf8')
+            const readStream = fs.createReadStream(__dirname + '/females.json', 'utf8');
             readStream.pipe(res);
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+        });
+        res.end('No Route')
+        
     }
     console.log(req.url);
-}
+})
+
 
 
 
 
 //server listen plus console log in terminal
-server.listen(3000, (msg) => {
-    console.log("Listening to port 3000");
+server.listen(3000, () => {
+    console.log(`Listening on port 3000`)
 });
