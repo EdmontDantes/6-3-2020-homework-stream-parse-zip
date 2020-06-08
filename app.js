aa73f726aa6cdd85bfefe64c35defaa85706e56e
Bogdan Kowaltchook <bogdan@En760.kowaltchook.com>
2020-06-07 22:50:08 -0400

    forgot to do inital commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+const csv = require('csv-parser');
+const zlib = require('zlib');
+
+
+//create empty arrays for male and female results to be used in csv conversion;
+const maleResults = [];
+const femaleResults = [];
+
+const writeStreamMales = fs.createWriteStream(__dirname + '/males.json', 'utf8')
+const writeStreamFemales = fs.createWriteStream(__dirname + '/females.json', 'utf8')
+
+fs.createReadStream('data.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+        if(data.Gender === 'M' || data.Gender === 'm') {
+            maleResults.push(data);
+        } else if (data.Gender === 'F' || data.Gender === 'm') {
+            femaleResults.push(data);
+        }
+    })
+    .on('end', () => {
+        writeStreamMales.write(JSON.stringify(maleResults));
+        writeStreamFemales.write(JSON.stringify(femaleResults));
+    });
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+        const readStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+        readStream.pipe(res);
+
+    } else if (req.url === '/') {
+            res.writeHead(200, { 
+                'Content-Type': 'text/html' 
+            });
+            const readStream = fs.createReadStream(__dirname + '/males.json', 'utf8')
+            readStream.pipe(res);
+    } else if (req.url === '/females') {
+
+            res.writeHead(200, { 
+                'Content-Type': 'text/html'
+            });
+            const readStream = fs.createReadStream(__dirname + '/females.json', 'utf8')
+            readStream.pipe(res);
+    }
+    console.log(req.url);
+}
+
+
+
+
+//server listen plus console log in terminal
+server.listen(3000, (msg) => {
+    console.log("Listening to port 3000");
+});
